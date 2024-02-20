4601f7a1795b5855cc119bb94ba6dd7b7730371a
Strigiformis <145602366+Strigiformis@users.noreply.github.com>
2024-02-20 15:38:45 +0100

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,15 @@
 let xp = 0;
 let health = 100;
 let gold = 50;
-let currentWeapon = 0;
+let currentWeapon,
+  startingWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
-
-const button1 = document.querySelector("#button1");
+const button1 = document.querySelector("#button1"); //Das erste Element mit der ID button1 wird in der Variable button1 gespeichert
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
-const text = document.querySelector("#text");
+const text = document.querySelector("#text"); //Das erste Element mit der ID text wird in der Variable text gespeichert
 const xpText = document.querySelector("#xpText");
 const healthText = document.querySelector("#healthText");
 const goldText = document.querySelector("#goldText");
@@ -17,17 +17,51 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 const invSlot = document.querySelector("#invSlot");
+
 const weapons = [
   { name: "stick", power: 5 },
   { name: "dagger", power: 30 },
   { name: "claw hammer", power: 50 },
   { name: "sword", power: 100 },
-];
+]; //Array mit Waffen und deren Werten
 const monsters = [
   { name: "slime", level: 2, health: 20 },
   { name: "fanged beast", level: 8, health: 60 },
   { name: "dragon", level: 20, health: 300 },
-];
+]; //Array mit Monstern und deren Werten
+const charClasses = [
+  {
+    name: "warrior",
+    str: 10,
+    dex: 5,
+    con: 10,
+    int: 5,
+    wis: 5,
+    cha: 5,
+    health: 100,
+  },
+  {
+    name: "mage",
+    str: 5,
+    dex: 5,
+    con: 5,
+    int: 10,
+    wis: 10,
+    cha: 5,
+    health: 80,
+  },
+  {
+    name: "rogue",
+    str: 5,
+    dex: 10,
+    con: 5,
+    int: 5,
+    wis: 5,
+    cha: 10,
+    health: 90,
+  },
+]; //Array mit Klassen und deren Werten
+
 const locations = [
   {
     name: "town square",
@@ -71,7 +105,7 @@ const locations = [
     name: "lose",
     "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
     "button functions": [restart, restart, restart],
-    text:  "You die. &#x2620;",
+    text: "You die. &#x2620;",
   },
   {
     name: "win",
@@ -79,20 +113,24 @@ const locations = [
     "button functions": [restart, restart, restart],
     text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;",
   },
-    {
-      name: "easter egg",
-      "button text": ["2", "8", "Go to town square?"],
-      "button functions": [pickTwo, pickEight, goTown],
-      text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!",
-    },
-  ];
-  
-
+  {
+    name: "easter egg",
+    "button text": ["2", "8", "Go to town square?"],
+    "button functions": [pickTwo, pickEight, goTown],
+    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!",
+  },
+];
 
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+function chooseChar() {
+  let vorName = prompt("Vorname: ");
+  let nachName = prompt("Nachname: ");
+  return vorName + " " + nachName;
+}
 
 function update(location) {
   monsterStats.style.display = `none`;
@@ -104,18 +142,35 @@ function update(location) {
   button3.onclick = location["button functions"][2];
   text.innerHTML = location.text;
   invSlot.innerText = displayInventory();
-  console.log(location.name, location.text, location["button text"], location["button functions"]
-  , button1.innerText, button2.innerText, button3.innerText, button1.onclick, button2.onclick, button3.onclick, invSlot.innerText, text.innerHTML, monsterStats.style.display);
-}
+  console.log(
+    location.name,
+    location.text,
+    location["button text"],
+    location["button functions"],
+    button1.innerText,
+    button2.innerText,
+    button3.innerText,
+    button1.onclick,
+    button2.onclick,
+    button3.onclick,
+    invSlot.innerText,
+    text.innerHTML,
+    monsterStats.style.display
+  );
+} //Funktion update() mit dem Parameter location wird definiert. Die Funktion aktualisiert die Textausgabe und die Buttons. Die Funktion wird
+//später in den anderen Funktionen aufgerufen, um die Textausgabe sowie Text und Funktion der Buttons zu aktualisieren.
+
 function displayInventory() {
   let inventoryText = "Inventory: ";
   for (let i = 0; i < inventory.length; i++) {
-    inventoryText += inventory[i];
+    //For-Schleife, die das Inventar durchläuft um die Waffen anzuzeigen
+    inventoryText += inventory[i]; //Die Waffe wird an den Text angehängt
     if (i < inventory.length - 1) {
-      inventoryText += ", ";
+      //Wenn die Waffe nicht die letzte im Inventar ist
+      inventoryText += ", "; //Werden ein Komma und ein Leerzeichen an den Text angehängt
     }
   }
-  return inventoryText
+  return inventoryText;
 }
 
 function goTown() {
@@ -146,7 +201,7 @@ function buyWeapon() {
     //The currentWeapon variable is the index of the weapons array, but array indexing starts at zero. The index of the last element in an array is one less than the length of the array.
     if (gold >= 30) {
       gold -= 30; //direkt mal 30 Gold abziehen
-      currentWeapon++; //Waffe upgraden indem der Zähler (currentWeapon) um 1 erhöht wird
+      currentWeapon++; //Waffe upgraden indem der Zähler (currentWeapon) um 1 erhöht und der Marker im Array auf das nächste Item gesetzt wird
       goldText.innerText = gold; //Goldanzeige aktualisieren
       let newWeapon = weapons[currentWeapon].name; //neue Waffe aus dem Inventar, sprich dem Array (weapons) holen
       text.innerText = "You now have a " + newWeapon + "."; //Textausgabe aktualisieren
@@ -196,49 +251,56 @@ function goFight() {
 }
 
 function attack() {
-  text.innerText = "The " + monsters[fighting].name + " attacks.";
-  text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
-  health -= getMonsterAttackValue(monsters[fighting].level);
+  text.innerText = "The " + monsters[fighting].name + " attacks."; //Textausgabe aktualisieren wenn der Spieler angreift
+  text.innerText +=
+    " You attack it with your " + weapons[currentWeapon].name + "."; //Textausgabe aktualisieren und die Waffe anzeigen
+  health -= getMonsterAttackValue(monsters[fighting].level); //Lebenspunkte des Spielers um den Wert des Angriffs des Monsters reduzieren
   if (isMonsterHit()) {
-    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
-  } else { 
-    text.innerText += " You miss.";
+    //Wenn das Monster getroffen wird (Funktion isMonsterHit() gibt true zurück)
+    monsterHealth -=
+      weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1; //Lebenspunkte des Monsters um den Wert der Waffe des Spielers reduzieren
+  } else {
+    text.innerText += " You miss."; //Textausgabe aktualisieren wenn der Spieler das Monster verfehlt
   }
-  healthText.innerText = health;
-  monsterHealthText.innerText = monsterHealth;
+  healthText.innerText = health; //Lebenspunkte des Spielers in der Anzeige aktualisieren
+  monsterHealthText.innerText = monsterHealth; //Lebenspunkte des Monsters in der Anzeige aktualisieren
   if (health <= 0) {
-    lose();
+    //Wenn die Lebenspunkte des Spielers kleiner oder gleich 0 sind
+    lose(); //Funktion lose() aufrufen
   } else if (monsterHealth <= 0) {
+    //Wenn die Lebenspunkte des Monsters kleiner oder gleich 0 sind
     if (fighting === 2) {
-      winGame();
+      //Wenn das Monster ein Drache ist
+      winGame(); //Funktion winGame() aufrufen
     } else {
-      defeatMonster();
+      defeatMonster(); //Funktion defeatMonster() aufrufen
     }
   }
   //Use the += operator to add " Your <weapon> breaks.", with a space in front of Your, to the end of text.innerText. Replace <weapon> with the last item in the inventory array using inventory.pop(), which will remove the last item in the array AND return it so it appears in your string.
   if (Math.random() > 0.1 && inventory.length !== 1) {
-    text.innerText += " Your " + inventory.pop() + " breaks.";
-    currentWeapon --;
+    //Wenn die Zufallszahl größer als 0.1 ist und das Inventar mehr als 1 Waffe enthält
+    text.innerText += " Your " + inventory.pop() + " breaks."; //Textausgabe aktualisieren und die zerbrochene Waffe anzeigen
+    currentWeapon--; //Waffe downgraden indem der Zähler (currentWeapon) um 1 verringert und der Marker im Array auf das vorherige Item gesetzt wird
   }
 }
 function getMonsterAttackValue(level) {
-  const hit = (level * 5 ) - (Math.floor(Math.random() * xp));
-  return hit > 0 ? hit : 0;
+  const hit = level * 5 - Math.floor(Math.random() * xp); // Der Wert des Angriffs des Monsters wird berechnet indem das Level des Monsters mit 5 multipliziert und von dem Ergebnis eine Zufallszahl zwischen 0 und der Anzahl der Erfahrungspunkte des Spielers abgezogen wird
+  return hit > 0 ? hit : 0; //Wenn der Wert des Angriffs des Monsters größer als 0 ist, wird der Wert über den befehl return an die Funktion zurückgegeben, ansonsten 0
 }
 function isMonsterHit() {
-  return Math.random() > 0.5 || health < 20;
+  return Math.random() > 0.5 || health < 20; //Wenn die Zufallszahl größer als 0.5 ist oder die Lebenspunkte des Spielers kleiner als 20 sind, gibt die Funktion true zurück, ansonsten false
 }
 
 function dodge() {
   text.innerText =
-    "You dodge the attack from the " + monsters[fighting].name + ".";
+    "You dodge the attack from the " + monsters[fighting].name + "."; //Textausgabe aktualisieren wenn der Spieler ausweicht
 }
 function defeatMonster() {
-  gold += Math.floor(monsters[fighting].level * 6.7);
-  xp += monsters[fighting].level;
+  gold += Math.floor(monsters[fighting].level * 6.7); //Gold um den Wert des Levels des Monsters mal 6.7 erhöhen
+  xp += monsters[fighting].level; //Erfahrungspunkte um den Wert des Levels des Monsters erhöhen
   goldText.innerText = gold;
   xpText.innerText = xp;
-  update(locations[4]);
+  update(locations[4]); //Funktion update() aufrufen und das Array locations an der Stelle 4 übergeben. damit wird die Textausgabe aktualisiert und die Buttons angepasst
 }
 function lose() {
   update(locations[0]);
@@ -246,7 +308,7 @@ function lose() {
 function winGame() {
   update(locations[6]);
 }
-function restart () {
+function restart() {
   xp = 0;
   health = 100;
   gold = 50;
@@ -256,12 +318,19 @@ function restart () {
   healthText.innerText = health;
   xpText.innerText = xp;
   goTown();
-} 
+}
 function easterEgg() {
   update(locations[7]);
 }
 function pick(guess) {
-
+  const numbers = [];
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+  for (let i = 0; i < 10; i++) {
+    text.innerText += numbers[i] + "\n";
+  }
 }
 function pickTwo() {
   pick(2);
